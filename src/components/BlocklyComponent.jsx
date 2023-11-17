50a411ff24760286e03de0021054bb2014178d13
Ayaan <ayaansiddiqui68@gmail.com>
2023-11-17 18:45:47 +0530

    Modified undo redo buttons
    Updated the button injection into blockly workspace following the updated format

--- src/components/BlocklyComponent.jsx
+++ src/components/BlocklyComponent.jsx
@@ -1,19 +1,19 @@
 // Updated BlocklyComponent.jsx
 import React, { useEffect, useRef } from 'react';
+import Blockly from 'blockly';
 import { Logic } from './BlockCategories/Logic';
 import { Loops } from './BlockCategories/Loops';
 import { Math } from './BlockCategories/Math';
 import { Text } from './BlockCategories/Text';
-import UndoRoundedIcon from '@mui/icons-material/UndoRounded';
-import RedoRoundedIcon from '@mui/icons-material/RedoRounded';
-import Blockly from 'blockly';
-import initializeBlockly from './InitializeBlockly';  
-import './BlocklyComponent.css';
+import initializeBlockly from './InitializeBlockly';  // import the function
 
 const BlocklyComponent = () => {
   const blocklyRef = useRef(null);
+
   useEffect(() => {
     if (blocklyRef.current === null) {
+      // Initialize Blockly with English
+        Blockly.setLocale('en');
       // Construct the complete toolbox XML
       const toolboxXml = `
         <xml id="toolbox" style="display: none">
@@ -28,40 +28,11 @@ const BlocklyComponent = () => {
     }
   }, []);
 
-  const handleUndo = () => {
-    const workspace = Blockly.getMainWorkspace();
-    if (workspace) {
-      workspace.undo();
-    }
-  };
-
-  const handleRedo = () => {
-    const workspace = Blockly.getMainWorkspace();
-    if (workspace) {
-      workspace.undo(true);
-    }
-  };
-
   return (
     <div style={{ width: '100%', height: '480px' }}>
       <h1 style={{ display: 'inline-block', fontSize: '14px', marginRight: '500px' }}>Blockly Toolbox</h1>
       <h1 style={{ display: 'inline-block', fontSize: '14px' }}>Blockly Workspace</h1>
-      
-      <div className="highlighted" id="blocklyDiv" style={{ height: '80%', width: '100%', position: 'relative' }}></div>
-
-      <div className='undoredo'>
-        <div className='circle-button' onClick={handleUndo}>
-          <button style={{ background: 'transparent', border: 'none', padding: 0 }}>
-            <UndoRoundedIcon style={{ color: '#d4d4d4' }} />
-          </button>
-        </div>
-        <div className='circle-button' onClick={handleRedo}>
-          <button style={{ background: 'transparent', border: 'none', padding: 0 }}>
-            <RedoRoundedIcon style={{ color: '#d4d4d4' }} />
-          </button>
-        </div>
-      </div>
-
+      <div className="highlighted" id="blocklyDiv" style={{ height: '100%', width: '100%', position: 'relative' }}></div>      
     </div>
   );
 };
--- src/components/InitializeBlockly.jsx
+++ src/components/InitializeBlockly.jsx
@@ -1,7 +1,7 @@
-import Blockly from 'blockly';
-
+import Blockly from "blockly/core";
+import "blockly/blocks";
 const InitializeBlockly = (toolboxXml) => {
-  return Blockly.inject('blocklyDiv', {
+  const workspace= Blockly.inject('blocklyDiv', {
     toolbox: toolboxXml,
     zoom: {
       controls: true,
@@ -25,6 +25,57 @@ const InitializeBlockly = (toolboxXml) => {
       wheel: true,
     },
   });
+  // Create custom zoom controls
+  const customUndoButton = createUndoButton(workspace, 30);
+  const customRedoButton = createRedoButton(workspace, 30);
+
+  // Append custom controls to Blockly's div
+  const blocklyDiv = workspace.getParentSvg().parentNode;
+  blocklyDiv.appendChild(customUndoButton.button);
+  blocklyDiv.appendChild(customRedoButton.button);
+
+  return workspace;
 };
+
+  // Helper functions to create undo, redo, zoom-in, zoom-out, and reset buttons
+  function createUndoButton(workspace, size) {
+  const button = document.createElement("button");
+  button.innerHTML = `<img src="/undo.svg" alt="Undo"> `;
+  button.addEventListener("click", () => {
+    workspace.undo();
+  });
+
+  setButtonStyles(button, size);
+  button.style.marginBottom = "10px"; // Adjust the position as needed
+  button.style.marginRight = "100px";
+  return { button };
+  }
+
+  function createRedoButton(workspace, size) {
+  const button = document.createElement("button");
+  button.innerHTML = `<img src="/redo.svg" alt="Redo"> `;
+  button.addEventListener("click", () => {
+    workspace.undo(true);
+  });
+
+  setButtonStyles(button, size);
+  button.style.marginBottom = "10px"; // Adjust the position as needed
+  button.style.marginRight = "65px";
+  return { button };
+  }
+
+  // Helper function to set common button styles
+  function setButtonStyles(button, size) {
+  button.style.zIndex = "1000";
+  button.style.position = "absolute";
+  button.style.visibility = "visible";
+  button.style.right = "10px";
+  button.style.bottom = "10px";
+  button.style.width = `${size}px`;
+  button.style.height = `${size}px`;
+  button.style.borderRadius = "50%";
+  button.style.backgroundColor = "transparent";
+  button.style.border = "2px solid #808080";``
+}
 
 export default InitializeBlockly;
